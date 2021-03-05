0620cb5cb78db87b06ef990960f9458f356ec2a8
Doniyorbek-Coder <79502140+Doniyorbek-Coder@users.noreply.github.com>
2021-03-05 20:23:15 +0500

    ...

--- main.js
+++ main.js
@@ -7,3 +7,48 @@ function vibratePattern() {
 }
 document.getElementById("result").innerHTML =
   navigator.deviceMemory || "unknown";
+var $status = document.getElementById("status");
+
+if ("Notification" in window) {
+  $status.innerText = Notification.permission;
+}
+
+function requestPermission() {
+  if (!("Notification" in window)) {
+    alert("Notification API not supported!");
+    return;
+  }
+
+  Notification.requestPermission(function (result) {
+    $status.innerText = result;
+  });
+}
+
+function nonPersistentNotification() {
+  if (!("Notification" in window)) {
+    alert("Notification API not supported!");
+    return;
+  }
+
+  try {
+    var notification = new Notification("Hi there - non-persistent!");
+  } catch (err) {
+    alert("Notification API error: " + err);
+  }
+}
+
+function persistentNotification() {
+  if (!("Notification" in window) || !("ServiceWorkerRegistration" in window)) {
+    alert("Persistent Notification API not supported!");
+    return;
+  }
+
+  try {
+    navigator.serviceWorker
+      .getRegistration()
+      .then((reg) => reg.showNotification("Hi there - persistent!"))
+      .catch((err) => alert("Service Worker registration error: " + err));
+  } catch (err) {
+    alert("Notification API error: " + err);
+  }
+}
