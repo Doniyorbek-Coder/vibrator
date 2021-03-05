f0761b8453ff592d1b1c2aa1058b642f446e6ac5
Doniyorbek-Coder <79502140+Doniyorbek-Coder@users.noreply.github.com>
2021-03-05 20:30:01 +0500

    ...

--- main.js
+++ main.js
@@ -52,3 +52,38 @@ function persistentNotification() {
     alert("Notification API error: " + err);
   }
 }
+function readBatteryLevel() {
+  var $target = document.getElementById("target");
+
+  if (!("bluetooth" in navigator)) {
+    $target.innerText = "Bluetooth API not supported.";
+    return;
+  }
+
+  navigator.bluetooth
+    .requestDevice({
+      filters: [
+        {
+          services: ["battery_service"],
+        },
+      ],
+    })
+    .then(function (device) {
+      return device.gatt.connect();
+    })
+    .then(function (server) {
+      return server.getPrimaryService("battery_service");
+    })
+    .then(function (service) {
+      return service.getCharacteristic("battery_level");
+    })
+    .then(function (characteristic) {
+      return characteristic.readValue();
+    })
+    .then(function (value) {
+      $target.innerHTML = "Battery percentage is " + value.getUint8(0) + ".";
+    })
+    .catch(function (error) {
+      $target.innerText = error;
+    });
+}
